353538d7b1df5608f278b9c9917f4cd10e96ee07
MarioCastillo <mariocastillosan@gmail.com>
2020-11-20 17:05:58 -0300

    Graficos parte 3

--- Conexión/app.py
+++ Conexión/app.py
@@ -25,26 +25,31 @@ def Graficos_Jorge():
 
 #Prueba de graficos con json
 def Graficos_Mario():
-    df_from_json=pd.read_json('../Templates/PCR_Regional.json')
+    df_from_json = pd.read_json('../Json/PCR_Regional.json')
     df_from_json['positividad'] = np.where(np.isnan(df_from_json['positividad']), 0, df_from_json['positividad'])
-    df_R=pd.read_json('../Json/Regiones_Provincias_comunas.json')
-    print(df_from_json[['Region','fecha','positividad']])
+    print(df_from_json[['Region', 'fecha', 'positividad']])
     st.write("""
     # From Json_PCR_Regional to Streamlit Table
     """
-    )
-    st.write(df_from_json[['Region','positividad','fecha']])
-    #print(df_R["region"])
-    options = list(df_R['region']) + ["Todas las regiones"]
-   # print(options)
-    region=st.sidebar.multiselect("Elegir regiones",options)
-    #print(region)
-    st.write(df_from_json[['Region','positividad','fecha']].loc[(df_from_json['Region'] == 'Atacama') & (df_from_json['positividad']>=0)])
+             )
+    st.write(df_from_json[['Region', 'positividad', 'fecha']])
+    region = st.sidebar.multiselect("Elegir regiones",
+                                    ["Atacama", "Ñuble", "Magallanes", "Arica y Parinacota", "Aysen", "Coquimbo",
+                                     "Araucanía", "Los Lagos", "Los Ríos", "Magallanes", "Tarapacá", "Valparaíso",
+                                     "Biobío", "O’Higgins", "Maule", "Metropolitana"])
+    for e in range (len(region)):
+        st.write(df_from_json[['Region','positividad','fecha']].loc[(df_from_json['Region'] ==str(region[e])) & (df_from_json['positividad']>=0)])
     chart_data = pd.DataFrame(
-
-        np.random.randn(20, 1), columns=[str(region)])
+        np.random.randn(20, len(region)), columns=region)
 
     st.line_chart(chart_data)
+
+    # Posibilida de graficar en base a un mapa pero faltaria latitud y longitud para las zonas
+    # map_data = pd.DataFrame(
+    #   np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    #  columns=['lat', 'lon'])
+
+    # st.map(map_data)
 
 Graficos_Mario()
 
